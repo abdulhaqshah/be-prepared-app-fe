b6a523bf5a3fc69428a30a64e6f2c2bd3ad324ea
abdulhaqshahg <abdulhaq.shah@southvillesol.com>
2019-04-17 12:54:16 +0500

    new changes

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -5,13 +5,13 @@ class Header extends Component{
         return (
 
             <nav id="navigation" className="navigation_nav-container">
-                <div className="navigation_nav-container">
-                    <a className="navigation-logo" href="/Home">Nav Bar</a>
-                    <div className="navigation-auth"> 
+                <li className="navigation_nav-container">
+                    <a className="navigation-logo" href="/Home">Be Prepared</a>
+                    <div className="navigation-auth" align="right"> 
                         <button ng-click="login()" className="navigation-auth_login-button" translate="navigation.connect.login">Log In</button> 
                         {/* <button ng-click="signup()" className="navigation-auth_signup-button" translate="navigation.connect.signup">Sign Up</button>  */}
                     </div>
-                </div>
+                </li>
             </nav>
             
             
