e806baa57ded4fa1fd5663128670d2a27674adba
Humna <humna.aleem@southvillesol.com>
2019-04-18 19:38:44 +0500

    done all required changes

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,28 +1,17 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 class Header extends Component {
   constructor(props) {
     super(props);
-    this.onClickLoginBtn = this.onClickLoginBtn.bind(this);
   }
-  onClickLoginBtn(e) {
-    this.props.history.push("/login");
-    e.preventDefault();
-  }
+
   render() {
     return (
       <nav id="navigation" className="navigation-nav-container">
-        <a className="navigation-logo" href="/Home">
-          Be Prepared
-        </a>
+        <a className="navigation-logo">Be Prepared</a>
 
         <div className="navigation-auth">
-          <button
-            className="navigation-auth-login-button"
-            onClick={this.onClickLoginBtn}
-          >
-            Log In
-          </button>
+          <button className="navigation-auth-login-button">Log In</button>
         </div>
       </nav>
     );
