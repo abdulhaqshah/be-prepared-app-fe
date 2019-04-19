fcd27235ef81efd35495b82700ee0d0ddff01831
Humna <humna.aleem@southvillesol.com>
2019-04-19 19:59:28 +0500

    forgot to add App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import React, { Component } from 'react';
 import Routes from "./routes/index";
 import 'font-awesome/css/font-awesome.min.css';
@@ -8,8 +7,6 @@ import './myOwn.css';
 class App extends Component {
   render() {
     return <Routes />;
-
-
   }
 }
 export default App;
