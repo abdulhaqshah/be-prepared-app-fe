41bf691a28a172c4a23dc8ee926807adabd11bf2
Humna <humna.aleem@southvillesol.com>
2019-04-19 20:05:48 +0500

    fogot to add App.js which is needed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 // import logo from './logo.svg';
 import Home from './Components/Home';
-import Login from './Components/Login';
+import SignUp from './Components/SignUp';
 import 'font-awesome/css/font-awesome.min.css';
 import './App.css';
 import './myOwn.css';
@@ -10,35 +10,12 @@ import './myOwn.css';
 class App extends Component {
   render() {
     return (
-      // <div className="App">
-      //   <header className="App-header">
-      //     <img src={logo} className="App-logo" alt="logo" />
-      //     <p>
-      //       Edit <code>src/App.js</code> and save to reload.
-      //     </p>
-      //     <a
-      //       className="App-link"
-      //       href="https://reactjs.org"
-      //       target="_blank"
-      //       rel="noopener noreferrer"
-      //     >
-      //       Be Prepered
-      //     </a>
-      //   </header>
-      // </div>
+      
       <Router>
         <div>
-          {/* <h2>Welcome to React Router Tutorial</h2>
-          <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <ul className="navbar-nav mr-auto">
-            <li><Link to={'/'} className="nav-link"> Home </Link></li>
-            <li><Link to={'/login'} className="nav-link">Login</Link></li>
-          </ul>
-          </nav> */}
-          {/* <hr /> */}
           <Switch>
               <Route exact path='/' component={Home} />
-              <Route path='/login' component={Login} />
+              <Route path='/SignUp' component={SignUp} />
           </Switch>
         </div>
       </Router>
