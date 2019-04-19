4f44e518a1319209a576e1706c55929d376d40ec
Humnaa <49718825+Humnaa@users.noreply.github.com>
2019-04-19 19:06:33 +0500

    fixed alignment of login btn and logo (#1)
    * fixed alignment of login btn and logo
    
    * done all required changes
    
    * Indentation and alignment changes have fixed
    
    * delete constructer to avoid warning
    
    * changed font size of login btn

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 // import logo from './logo.svg';
 import Home from './Components/Home';
 import Login from './Components/Login';
@@ -10,22 +10,6 @@ import './myOwn.css';
 class App extends Component {
   render() {
     return (
-      // <div className="App">
-      //   <header className="App-header">
-      //     <img src={logo} className="App-logo" alt="logo" />
-      //     <p>
-      //       Edit <code>src/App.js</code> and save to reload.
-      //     </p>
-      //     <a
-      //       className="App-link"
-      //       href="https://reactjs.org"
-      //       target="_blank"
-      //       rel="noopener noreferrer"
-      //     >
-      //       Be Prepered
-      //     </a>
-      //   </header>
-      // </div>
       <Router>
         <div>
           {/* <h2>Welcome to React Router Tutorial</h2>
