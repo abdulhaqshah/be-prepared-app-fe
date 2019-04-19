7ecb10e90ef45d27a8447d8ff95b33c87ef89ed7
Humna <humna.aleem@southvillesol.com>
2019-04-19 11:38:43 +0500

    Final login form

--- src/containers/Login/Login.js
+++ src/containers/Login/Login.js
@@ -1,20 +1,75 @@
-import React, {Component} from 'react';
-
-class Login extends Component{
-  constructor(props){
+import React, { Component } from "react";
+import "./Login.css";
+import Header from "./Header";
+import Footer from "./Footer";
+class Login extends Component {
+  constructor(props) {
     super(props);
-    // this.props.children;
+    this.state = {
+      loginUserName: "",
+      loginPass: ""
+    };
+    this.setLoginUserName = this.setLoginUserName.bind(this);
+    this.setLoginPassword = this.setLoginPassword.bind(this);
+    this.loginValidation = this.loginValidation.bind(this);
   }
-    render() {
-        return (
-
-            <div>
-                This will be a login page of the application
-            </div>
-
-        )
+  setLoginUserName(event) {
+    this.setState({ loginUserName: event.target.value });
+  }
+  setLoginPassword(event) {
+    this.setState({ loginPass: event.target.value });
+  }
+  loginValidation(e) {
+    e.preventDefault();
+    if (this.state.loginUserName === "" || this.state.loginPass === "") {
+      alert("All Feilds Required");
+    } else {
+      this.props.history.push("/Home");
     }
-
+  }
+  render() {
+    return (
+      <div>
+        <Header />
+        <div className="outer-container">
+          <div className="container">
+            <h1 className="heading" align="center">
+              Login
+            </h1>{" "}
+            <br />
+            <label className="labels">UserName</label>
+            <br />
+            <input
+              className="field"
+              name="loginUserName"
+              type="text"
+              placeholder="UserName"
+              onChange={this.setLoginUserName}
+            />
+            <br /> <br />
+            <label className="labels">Password</label>
+            <br />
+            <input
+              className="field"
+              name="loginPassword"
+              type="password"
+              placeholder="Password"
+              onChange={this.setLoginPassword}
+            />
+            <br /> <br /> <br />
+            <button
+              className="button"
+              name="loginBtn"
+              type="submit"
+              onClick={this.loginValidation}
+            >
+              Login
+            </button>
+          </div>
+        </div>
+        <Footer />
+      </div>
+    );
+  }
 }
-
-export default Login
+export default Login;
