70dd68f86a45c6c96bbfc928c64d2555dbea3536
Humna <humna.aleem@southvillesol.com>
2019-04-19 12:45:22 +0500

    Indentation and alignment changes have fixed

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -8,7 +8,7 @@ class Header extends Component {
   render() {
     return (
       <nav id="navigation" className="navigation-nav-container">
-        <a className="navigation-logo">Be Prepared</a>
+        <a className="navigation-logo" href="/Home">Be Prepared</a>
 
         <div className="navigation-auth">
           <button className="navigation-auth-login-button">Log In</button>
