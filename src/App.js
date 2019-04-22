3dbcbd6d3b5968ef0bbe0551a0583b238ae8a979
Ahsan Yousaf <ahsan.yousaf@southvillesol.com>
2019-04-22 17:02:46 +0500

    setup project structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 // import logo from './logo.svg';
-import Home from './Components/Home';
-import Login from './Components/Login';
+import Home from './containers/Home/Home';
+import Login from './containers/Login/Login';
 import 'font-awesome/css/font-awesome.min.css';
 import './App.css';
 import './myOwn.css';
--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,8 +1,8 @@
 
 import React, {Component} from 'react';
-import Header from './Header';
+import Header from '../../components/Header';
 import Section from './Section';
-import Footer from './Footer';
+import Footer from '../../components/Footer';
 
 class Home extends Component{
     render() {
