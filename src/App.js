2dd63fa745a3b122f4379d2142a7c52a87170314
Humna <humna.aleem@southvillesol.com>
2019-04-22 13:18:36 +0500

    Modified version of signup form

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 // import logo from './logo.svg';
 import Home from './Components/Home';
+// import Login from './Components/Login';
 import SignUp from './Components/SignUp';
+
 import 'font-awesome/css/font-awesome.min.css';
 import './App.css';
 import './myOwn.css';
@@ -22,7 +24,7 @@ class App extends Component {
           {/* <hr /> */}
           <Switch>
               <Route exact path='/' component={Home} />
-              <Route path='/SignUp' component={SignUp} />
+              <Route path='/signup' component={SignUp} />
           </Switch>
         </div>
       </Router>
