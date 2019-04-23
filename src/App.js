3f1f8a4670760042b56d46d37f686a707a50fcbe
abdulhaqshahg <abdulhaq.shah@southvillesol.com>
2019-04-23 15:50:22 +0500

    previous component folder removed

--- src/App.js
+++ src/App.js
@@ -22,14 +22,6 @@ class App extends Component {
     return (
       <Router>
         <div>
-          {/* <h2>Welcome to React Router Tutorial</h2>
-          <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <ul className="navbar-nav mr-auto">
-            <li><Link to={'/'} className="nav-link"> Home </Link></li>
-            <li><Link to={'/login'} className="nav-link">Login</Link></li>
-          </ul>
-          </nav> */}
-          {/* <hr /> */}
           <Switch>
             <Route exact path="/" component={Home} />
             <Route path="/login" component={Login} />
