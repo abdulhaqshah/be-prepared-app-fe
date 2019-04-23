de9db6a068a3366bd0576c3b1f66192a32dbfa29
abdulhaqshahg <abdulhaq.shah@southvillesol.com>
2019-04-23 17:48:19 +0500

    routing sperated into different file and constant used in different files

--- src/App.js
+++ src/App.js
@@ -1,33 +1,17 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 // import logo from './logo.svg';
-<<<<<<< HEAD
 import Home from "./containers/Home/Home";
 import Login from "./containers/Login/Login";
 import Login from "./containers/SignUp/SignUp";
 import "font-awesome/css/font-awesome.min.css";
 import "./App.css";
-=======
-import Login from './containers/Login/Login';
-import Home from './containers/Home/Home';
-// import Login from './Components/Login';
-import SignUp from './containers/SignUp/SignUp';
-import 'font-awesome/css/font-awesome.min.css';
-import './App.css';
-import './myOwn.css';
+import Routes from "./routes/index";
+import "./myOwn.css";
 
->>>>>>> remove conflicts
 class App extends Component {
   render() {
-    return (
-      <Router>
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/login" component={Login} />
-            <Route path="/signup" component={SignUp} />
-          </Switch>
-      </Router>
-    );
+    return <Routes />;
   }
 }
 
