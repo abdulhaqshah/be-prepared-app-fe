5e4bf421871565ee31358ede6121ad8bd29f9d87
Humna <humna.aleem@southvillesol.com>
2019-04-23 15:35:07 +0500

    remove conflicts

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 // import logo from './logo.svg';
+<<<<<<< HEAD
 import Home from "./containers/Home/Home";
 import Login from "./containers/Login/Login";
 import Login from "./containers/SignUp/SignUp";
 import "font-awesome/css/font-awesome.min.css";
 import "./App.css";
+=======
+import Login from './containers/Login/Login';
+import Home from './containers/Home/Home';
+// import Login from './Components/Login';
+import SignUp from './containers/SignUp/SignUp';
+import 'font-awesome/css/font-awesome.min.css';
+import './App.css';
+import './myOwn.css';
+
+>>>>>>> remove conflicts
 class App extends Component {
   render() {
     return (
