0f056369810ea67912d7394b0dc6e8999b7531ff
abdulhaqshahg <abdulhaq.shah@southvillesol.com>
2019-04-23 17:50:17 +0500

    ./routes/index changed to ./routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import React, { Component } from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 // import logo from './logo.svg';
-import Home from "./containers/Home/Home";
-import Login from "./containers/Login/Login";
-import Login from "./containers/SignUp/SignUp";
-import "font-awesome/css/font-awesome.min.css";
-import "./App.css";
 import Routes from "./routes/index";
 import "./myOwn.css";
 
+import React, { Component } from "react";
+import Routes from "./routes";
+import "font-awesome/css/font-awesome.min.css";
+import "./App.css";
+import "./myOwn.css";
 class App extends Component {
   render() {
     return <Routes />;
