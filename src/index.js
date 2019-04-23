d306d8f4cdda49b092cdc0af473970a2f90d09b9
Humna <humna.aleem@southvillesol.com>
2019-04-23 18:31:04 +0500

    pass store as a prop so that store is available to every component

--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { Provider } from 'redux';
-import {createStore} from 'react-redux';
+import { Provider } from 'redux-redux';
+import {createStore} from 'react';
+import Reducer from './store/Reducer';
 
-const store = createStore();
+const store = createStore(Reducer);
 //allow us to inject the global store 
-ReactDOM.render(<Provider><App /></Provider>, document.getElementById('root'));
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
