b4fe5dd4d8894048d7f3b59cdc651e9421f31928
abdulhaqshahg <abdulhaq.shah@southvillesol.com>
2019-04-29 16:38:31 +0500

    merge conflict removed in develop

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 // import logo from './logo.svg';
-<<<<<<< HEAD
 import Home from "./containers/Home/Home";
 import Login from "./containers/Login/Login";
 import "font-awesome/css/font-awesome.min.css";
 import "./App.css";
 import "./myOwn.css";
-=======
-<<<<<<< HEAD
-import Login from './containers/Login/Login';
-=======
-import Home from './Components/Home';
-import SignUp from './Components/SignUp';
->>>>>>> forgot to add App.js
-import 'font-awesome/css/font-awesome.min.css';
-import './App.css';
-import './myOwn.css';
->>>>>>> forgot to add App.js
 
 class App extends Component {
   render() {
@@ -33,13 +21,8 @@ class App extends Component {
           </nav> */}
           {/* <hr /> */}
           <Switch>
-<<<<<<< HEAD
             <Route exact path="/" component={Home} />
             <Route path="/login" component={Login} />
-=======
-              <Route exact path='/' component={Home} />
-              <Route path='/SignUp' component={SignUp} />
->>>>>>> forgot to add App.js
           </Switch>
         </div>
       </Router>
