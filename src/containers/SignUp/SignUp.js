ac62414c6fd10992bbe3c0d72bb63edb65a73fb1
Humna <humna.aleem@southvillesol.com>
2019-04-29 20:02:25 +0500

    install axios for api integration and try to post data

--- src/containers/SignUp/SignUp.js
+++ src/containers/SignUp/SignUp.js
@@ -3,6 +3,7 @@ import SimpleReactValidator from "simple-react-validator";
 import "./SignUp.css";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
+import axios from "axios";
 
 class SignUp extends Component {
   constructor(props) {
@@ -10,12 +11,13 @@ class SignUp extends Component {
     this.validator = new SimpleReactValidator({
       //This rule is for confirm password
       validators: {
-        cp: {  // name the rule
-          message: 'The :attribute does not match.',
+        cp: {
+          // name the rule
+          message: "The :attribute does not match.",
           rule: (val, params, validator) => {
             return Boolean(val) ? val === params[0] : null;
           },
-          required: true  // optional
+          required: true // optional
         }
       }
     });
@@ -23,22 +25,64 @@ class SignUp extends Component {
       name: "",
       email: "",
       password: "",
-      confirmPassword: ""
+      confirmPassword: "",
+      loading: false,
+      message: ""
     };
-    this.handleUserInput =  this.handleUserInput.bind(this);
+    this.handleUserInput = this.handleUserInput.bind(this);
     this.submitForm = this.submitForm.bind(this);
+    this.loadOrShowMsg = this.loadOrShowMsg.bind(this);
+    this.postData = this.postData.bind(this);
   }
- 
+
   handleUserInput(e) {
     const name = e.target.name;
     const value = e.target.value;
-    this.setState({[name]: value});
+    this.setState({ [name]: value });
+  }
+  postData(event) {
+    event.preventDefault();
+    const name = this.state.name;
+    const email = this.state.email;
+    const pass = this.state.password;
+    const confirmPass = this.confirmPassword;
+    this.setState({
+      loading: true
+    });
+    const data = {
+      name,
+      email,
+      pass,
+      confirmPass
+    };
+    axios
+      .post("localhost:8000/user/register", data)
+      .then(Response => {
+        console.log(Response);
+        this.setState({
+          loading: false,
+          message: Response.data
+        });
+      })
+      .catch(err => {
+        console.log(err);
+        this.setState({
+          loading: false
+        });
+      });
   }
 
+  loadOrShowMsg() {
+    if (this.state.loading) {
+      return <p>Loading....</p>;
+    } else {
+      return <p>{this.state.message}</p>;
+    }
+  }
   submitForm(e) {
     e.preventDefault();
     if (this.validator.allValid()) {
-      alert('You submitted the form and stuff!');
+      alert("You submitted the form and stuff!");
     } else {
       this.validator.showMessages();
       // rerender to show messages for the first time
@@ -52,30 +96,70 @@ class SignUp extends Component {
         <Header />
         <div className="signup-container">
           <div className="inner-container">
-            <h1 className="heading" align="center">Sign Up</h1>
+            <h1 className="heading" align="center">
+              Sign Up
+            </h1>
             <form onSubmit={this.submitForm}>
               <div>
                 <label className="labels vertical-spacing"> Your name </label>
                 <br />
-                <input className="field" name="name" type="text" onChange={this.handleUserInput}/>
+                <input
+                  className="field"
+                  name="name"
+                  type="text"
+                  onChange={this.handleUserInput}
+                />
               </div>
               <div>
-                <label className="labels vertical-spacing">Email <span className="required-indicator">*</span></label>
+                <label className="labels vertical-spacing">
+                  Email <span className="required-indicator">*</span>
+                </label>
                 <br />
-                <input className="field" name="email" type="email" onChange={this.handleUserInput}/>
-                {this.validator.message('email', this.state.email, 'required|email')}
+                <input
+                  className="field"
+                  name="email"
+                  type="email"
+                  onChange={this.handleUserInput}
+                />
+                {this.validator.message(
+                  "email",
+                  this.state.email,
+                  "required|email"
+                )}
               </div>
               <div>
-                <label className="labels vertical-spacing">Password <span className="required-indicator">*</span></label>
+                <label className="labels vertical-spacing">
+                  Password <span className="required-indicator">*</span>
+                </label>
                 <br />
-                <input className="field" name="password" type="password" onChange={this.handleUserInput}/>
-                {this.validator.message('password', this.state.password, 'required|min:6|max:20')}
+                <input
+                  className="field"
+                  name="password"
+                  type="password"
+                  onChange={this.handleUserInput}
+                />
+                {this.validator.message(
+                  "password",
+                  this.state.password,
+                  "required|min:6|max:20"
+                )}
               </div>
               <div>
-                <label className="labels vertical-spacing">Confirm Password <span className="required-indicator">*</span></label>
+                <label className="labels vertical-spacing">
+                  Confirm Password <span className="required-indicator">*</span>
+                </label>
                 <br />
-                <input className="field" name="confirmPassword" type="password" onChange={this.handleUserInput}/>
-                {this.validator.message('confirmPassword', this.state.confirmPassword, 'requried|cp:'+this.state.password)}
+                <input
+                  className="field"
+                  name="confirmPassword"
+                  type="password"
+                  onChange={this.handleUserInput}
+                />
+                {this.validator.message(
+                  "confirmPassword",
+                  this.state.confirmPassword,
+                  "requried|cp:" + this.state.password
+                )}
               </div>
               <div className="term-conditions">
                 By clicking 'Create account', you agree to our Terms and
@@ -83,10 +167,16 @@ class SignUp extends Component {
                 and Disclosure Guidelines.
               </div>
               <div>
-                <button className="button" name="signUpBtn" type="submit">
+                <button
+                  className="button"
+                  name="signUpBtn"
+                  type="submit"
+                  onClick={this.postData}
+                >
                   Create Account
                 </button>
               </div>
+              {this.loadOrShowMsg}
             </form>
           </div>
         </div>
