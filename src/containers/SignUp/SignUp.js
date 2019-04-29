86d94093ce28d12e162825bf72e0009bf367ceec
abdulhaqshahg <abdulhaq.shah@southvillesol.com>
2019-04-29 15:55:30 +0500

    validation library added in the signup

--- src/containers/SignUp/SignUp.js
+++ src/containers/SignUp/SignUp.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import SimpleReactValidator from "simple-react-validator";
 import "./SignUp.css";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
@@ -6,71 +7,60 @@ import Footer from "../../components/Footer";
 class SignUp extends Component {
   constructor(props) {
     super(props);
+    this.validator = new SimpleReactValidator({
+      //This rule is for confirm password
+      validators: {
+        cp: {  // name the rule
+          message: 'The :attribute does not match.',
+          rule: (val, params, validator) => {
+            debugger
+            return Boolean(val) ? val === params[0] : null;
+            // return validator.helpers.testRegex(val,/^(?!0)(?!.*\.$)((1?\d?\d|25[0-5]|2[0-4]\d)(\.|$)){4}$/i) && params.indexOf(val) === -1
+          },
+          //messageReplace: (message, params) => message.replace(':values', this.helpers.toSentence(params)),  // optional
+          required: true  // optional
+        }
+      }
+    });
     this.state = {
       name: "",
       email: "",
       password: "",
-      confirmPassword: "",
-      emailErrorMessage:"",
-      passwordErrorMessage:"",
-      formValid: false,
-      emailValid: false,
-      passwordValid: false
+      confirmPassword: ""
     };
     this.handleUserInput =  this.handleUserInput.bind(this);
     this.submitForm = this.submitForm.bind(this);
-    this.showValidationError = this.showValidationError.bind(this);
   }
  
   handleUserInput(e) {
     const name = e.target.name;
     const value = e.target.value;
     this.setState({[name]: value});
-    this.validateField(name, value);
+    this.checkValidation();
+  }
+
+  checkValidation() {
+    debugger
+    if (this.validator.allValid()) {
+      alert('You submitted the form and stuff!');
+    } else {
+      this.validator.showMessages();
+      // rerender to show messages for the first time
+      this.forceUpdate();
+    }
   }
 
   submitForm(e) {
     e.preventDefault();
-  }
-
-  validateField(fieldName, value) {
-    let emailValid = this.state.emailValid;
-    let passwordValid = this.state.passwordValid;
-  
-    switch (fieldName) {
-      case 'email':
-        emailValid = this.validateEmail(value);
-        this.setState({emailErrorMessage : emailValid ? '' : 'email is invalid'});
-        break;
-      case 'password':
-        passwordValid = value.length >= 6;
-        this.setState({passwordErrorMessage : passwordValid ? '': ' password is too short'});
-        break;
-      default:
-        break;
-    }
-    this.validateForm();
-  }
-  
-  validateForm() {
-    this.setState({formValid: this.state.emailValid && this.state.passwordValid});
-  }
-
-  validateEmail(email) {
-    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
-  }
-
-  showValidationError(field) {
-    switch (field) {
-      case "email":
-      return ( !this.state.emailValid ? <label>{this.state.emailErrorMessage}</label> : null)
-      case "password":
-      return ( !this.state.passwordValid ? <label>{this.state.passwordErrorMessage}</label> : null)
-      default:
-      break
+    if (this.validator.allValid()) {
+      alert('You submitted the form and stuff!');
+    } else {
+      this.validator.showMessages();
+      // rerender to show messages for the first time
+      this.forceUpdate();
     }
   }
+
   render() {
     return (
       <div>
@@ -78,7 +68,7 @@ class SignUp extends Component {
         <div className="signup-container">
           <div className="inner-container">
             <h1 className="heading" align="center">Sign Up</h1>
-            <form onSubmit={this.submitHandler}>
+            <form onSubmit={this.submitForm}>
               <div>
                 <label className="labels vertical-spacing"> Your name </label>
                 <br />
@@ -87,19 +77,20 @@ class SignUp extends Component {
               <div>
                 <label className="labels vertical-spacing">Email <span className="required-indicator">*</span></label>
                 <br />
-                <input className="field" name="email"type="text" onChange={this.handleUserInput}/>
-                {this.showValidationError("email")}
+                <input className="field" name="email" type="email" onChange={this.handleUserInput}/>
+                {this.validator.message('email', this.state.email, 'required|email')}
               </div>
               <div>
                 <label className="labels vertical-spacing">Password <span className="required-indicator">*</span></label>
                 <br />
                 <input className="field" name="password" type="password" onChange={this.handleUserInput}/>
-                {this.showValidationError("password")}
+                {this.validator.message('password', this.state.password, 'required|min:6|max:20')}
               </div>
               <div>
                 <label className="labels vertical-spacing">Confirm Password <span className="required-indicator">*</span></label>
                 <br />
                 <input className="field" name="confirmPassword" type="password" onChange={this.handleUserInput}/>
+                {this.validator.message('confirmPassword', this.state.confirmPassword, 'required|min:6|max:20|cp:'+this.state.password)}
               </div>
               <div className="term-conditions">
                 By clicking 'Create account', you agree to our Terms and
@@ -107,7 +98,7 @@ class SignUp extends Component {
                 and Disclosure Guidelines.
               </div>
               <div>
-                <button className="button" name="signUpBtn" type="submit" disabled={!this.state.formValid}>
+                <button className="button" name="signUpBtn" type="submit">
                   Create Account
                 </button>
               </div>
