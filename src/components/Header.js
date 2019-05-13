9b4fe43abf187f72ebb4a3f6c4607f161c6989be
Humna <humna.aleem@southvillesol.com>
2019-05-13 15:57:57 +0500

    remove unused keyword

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
-import {LOGIN, HOME} from '../constants';
+import {HOME} from '../constants';
 
 class Header extends Component {
   render() {
