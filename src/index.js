f6adb83fa603cc8c80d4a179594c3272aa4333e8
Humna <humna.aleem@southvillesol.com>
2019-05-14 14:45:05 +0500

    import library for bootstrap in index.js

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
 import {createStore} from 'redux';
 import Reducer from './store/reducers/Reducer';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const store = createStore(Reducer);//store should b at high level so index.js is at higher level
 //allow us to inject the global store 
