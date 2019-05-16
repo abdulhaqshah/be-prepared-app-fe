9b8ec602eae5d83d4c8f503b0bae6d37d269e9c3
Humna <humna.aleem@southvillesol.com>
2019-05-16 16:57:31 +0500

    made structure of footer

--- src/containers/Dashboard/Dashboard.js
+++ src/containers/Dashboard/Dashboard.js
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
 import NavBar from "../Dashboard/NavBar";
+import DashboardFooter from "../Dashboard/DashboardFooter";
+
 class DashBoard extends Component {
   render() {
     return (
       <div>
         <NavBar />
+        <DashboardFooter/>
       </div>
     );
   }
