a94cce89acf86ce7f5af06efe88889109c8c4eea
Humna <humna.aleem@southvillesol.com>
2019-05-16 16:27:24 +0500

    align icons in sidebar

--- src/containers/Dashboard/Dashboard.js
+++ src/containers/Dashboard/Dashboard.js
@@ -1,13 +1,10 @@
 import React, { Component } from "react";
 import NavBar from "../Dashboard/NavBar";
-import SideBar from "./SideBar";
-
 class DashBoard extends Component {
   render() {
     return (
       <div>
         <NavBar />
-        <SideBar />
       </div>
     );
   }
--- src/containers/Dashboard/SideBar.js
+++ src/containers/Dashboard/SideBar.js
@@ -1,48 +1,49 @@
 import React, { Component } from "react";
-// import { Link } from "react-router-dom";
-// import { HOME } from "../../constants";
 import "./Sidebar.scss";
 
 class SideBar extends Component {
   render() {
     return (
-      <nav class="sidenav">
-        <div class="sidebar-header">
-          <h3>SideBar</h3>
+      <div className="sidebar-container">
+          <nav class="sidenav">
+        <div class="sidebar-header mr-4">
+          <h3 align="center">SideBar</h3>
         </div>
         <ul class="list-unstyled components">
           <li>
             <a href="/home">
-              <i className="fa fa-book" /> Courses
+              <i className="fa fa-book mr-2" /> Courses
             </a>
           </li>
           <li>
             <a href="/home">
-              <i className="fa fa-language" /> Languages
+              <i className="fa fa-language mr-2" /> Languages
             </a>
           </li>
           <li>
             <a href="/home">
-              <i className="fa  fa-question" /> Genral Questions
+              <i className="fa fa-question mr-2 fa-lg" /> Genral Questions
             </a>
           </li>
           <li>
             <a href="/home">
-              <i className="fa  fa-file-text-o" /> Analytical Questions
+              <i className="fa fa-file-text-o mr-2" /> Analytical Questions
             </a>
           </li>
           <li>
             <a href="/home">
-              <i className="fa  fa-file" /> CV Templates
+              <i className="fa fa-file mr-2" /> CV Templates
             </a>
           </li>
           <li>
             <a href="/home">
-              <i className="fa fa-lightbulb-o" /> Interview Tips
+              <i className="fa fa-lightbulb-o mr-3 fa-lg" /> Interview Tips
             </a>
           </li>
         </ul>
       </nav>
+      </div>
+    
     );
   }
 }
