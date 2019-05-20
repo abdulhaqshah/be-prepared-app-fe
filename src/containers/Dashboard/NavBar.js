9c21bf46c7a7fb0da8e0e34b49e645c46770f0e7
Humna <humna.aleem@southvillesol.com>
2019-05-20 13:38:56 +0500

    remove toggle button from navbar

--- src/containers/Dashboard/NavBar.js
+++ src/containers/Dashboard/NavBar.js
@@ -5,32 +5,9 @@ import "./Navbar.scss";
 import SideBar from "./SideBar";
 
 class NavBar extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      show: false
-    };
-    this.toggle = this.toggle.bind(this);
-  }
-  toggle = () =>{
-    this.setState({
-      show: !this.state.show
-    })
-  }
   render() {
     return (
       <nav class="navbar navbar-expand-lg fixed-top navbar-dark bg-dark shadow-lg">
-        <button
-          class="btn"
-          onClick={this.toggle}
-        >
-          <i class="fa fa-bars fa-2x" />
-        </button>
-        <div>
-          {
-            this.state.show && ( <SideBar />)
-          } 
-        </div>
         <div>
           <Link className="navigation-logo" to={HOME}>
             Be Prepared
