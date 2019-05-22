91d1fd2acc6ad966962854e8672e0b0694d99d0a
Humna <humna.aleem@southvillesol.com>
2019-05-22 13:49:44 +0500

    make a card component for code reuseability

---
+++ src/containers/Dashboard/Card.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import "./Card.scss";
+class Card extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div className="courses-card-div ">
+        <div
+          className="card-div shadow-lg"
+          className="card"
+          style={{ width: "23rem", height: "30rem" }}
+        >
+          <div className="card-body">
+            <p className="text-muted">{this.props.title}</p>
+            <hr />
+            <a href="#">OOP</a>
+            <hr />
+            <a href="#">Data Structures</a>
+            <hr />
+            <a href="#">Database</a>
+            <hr />
+            <a href="#">Algorithm</a>
+            <hr />
+            <a href="#">Web Development</a>
+            <hr />
+            <a href="#">Android</a>
+            <br />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Card;
