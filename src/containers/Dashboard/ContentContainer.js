1fb0ede8b2d038c628018ad9e5baeb58368f1168
Humna <humna.aleem@southvillesol.com>
2019-05-22 13:49:08 +0500

    make rough structure of dashboard modified version

--- src/containers/Dashboard/ContentContainer.js
+++ src/containers/Dashboard/ContentContainer.js
@@ -1,35 +1,108 @@
 import React, { Component } from "react";
-import {
-  Container,
-  Button,
-  lightColors,
-  darkColors
-} from "react-floating-action-button";
-import "./ContentContainer.scss"
+import "./ContentContainer.scss";
+import Card from "./Card";
 
 class ContentContainer extends Component {
   constructor(props) {
     super(props);
-    this.takeQuiz = this.takeQuiz.bind(this);
   }
-  takeQuiz = () => {};
   render() {
     return (
       <div className="content-container">
-        <p>This is main content container</p>
-        <div className="well well-lg">Large Well</div>
+        <header>
+          <div className="prac-div">
+            <span>practice</span>
+          </div>
+          <div className="dashboard-heading-div font-weight-bold">
+            <h5 className="heading">Dashboard</h5>
+          </div>
+        </header>
+        <div className="main-container">
+          <h5>Your Skills</h5>
+          <div className="interview-card1">
+            <div
+              className="card-div shadow-lg"
+              className="card"
+              style={{ width: "35rem", height: "15rem" }}
+            >
+              <div className="card-body">
+                <p>INTERVIEW PREPARATION</p>
+                <h2 className="card-title">Interview Preparation Kit</h2>
+                <p className="card-text text-muted">
+                  curated challenges and tips based on learnings from 1000+
+                  compaanies to help you prepare for your upcoming interviews.
+                </p>
+                <a href="#" className="btn btn-success btn-lg">
+                  View
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="interview2-card-div ">
+            <div
+              className="card-div shadow"
+              className="card"
+              style={{ width: "35rem", height: "15rem" }}
+            >
+              <div className="card-body">
+                <div className="icon-div">
+                  <i className="fa fa-envelope-o fa-lg" />
+                </div>
+                <h3 className="card-title">Confirm your email address </h3>
+                <p className="card-text ">
+                  please check your inbox. We've sent a link to confirm your
+                  email address. Once you confir, you will be able to access all
+                  the features of out site, including
+                  <b> Jobs, Leaderboard, Recommendations and more.</b>
+                </p>
+                <a href="#" className="btn btn-success btn-lg ">
+                  Resend Confirmation Email
+                </a>
+              </div>
+            </div>
+          </div>
 
-        <Container>
-          <Button
-            tooltip="Take Quiz!"
-            icon="fa fa-pencil "
-            styles={{
-              backgroundColor: darkColors.blue,
-              color: lightColors.white
-            }}
-            onClick={this.takeQuiz}
-          />
-        </Container>
+          <div className="interview3-card-div shadow-lg">
+            <div
+              className="card-div"
+              className="card"
+              style={{ width: "35rem", height: "15rem" }}
+            >
+              <div className="card-body">
+                <p>PROBLEM SOLVING</p>
+                <h2 className="card-title">Problem Solving</h2>
+                <div className="progress-div">
+                  <div class="progress">
+                    <div
+                      class="progress-bar"
+                      role="progressbar"
+                      style={{ width: " 25%" }}
+                      aria-valuenow="25"
+                      aria-valuemin="0"
+                      aria-valuemax="100"
+                    />
+                  </div>
+                </div>
+
+                <a href="#" className="btn btn-outline-success btn-lg">
+                  Continue Practice
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="heading-div">
+            <h5 className="text-muted">Explore BePrepared Skills</h5>
+          </div>
+          <div className="courses">
+          </div>
+          <Card title="POBLEM SOLVING"/>
+          <div className="languages">
+          <Card title="LANGUAGES"/>
+          </div> 
+          <div className="specialized-skills">
+          <Card title="SPECIALIZED SKILLS "/>
+          </div> 
+        </div>
       </div>
     );
   }
--- src/containers/Dashboard/Dashboard.js
+++ src/containers/Dashboard/Dashboard.js
@@ -2,14 +2,12 @@ import React, { Component } from "react";
 import Navbar from "./Navbar";
 import DashboardFooter from "../Dashboard/DashboardFooter";
 import ContentContainer from "../Dashboard/ContentContainer";
-import Sidebar from "./Sidebar";
 
 class Dashboard extends Component {
   render() {
     return (
       <div>
         <Navbar />
-        <Sidebar />
         <ContentContainer />
         <DashboardFooter />
    
