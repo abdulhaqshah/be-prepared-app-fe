08c072035d0b2e5e24db22e7d9cffaacace80ee4
Humna <humna.aleem@southvillesol.com>
2019-05-27 15:33:41 +0500

    remove extra fragments

--- src/containers/Dashboard/ContentContainer.js
+++ src/containers/Dashboard/ContentContainer.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from "react";
+import React, { Component} from "react";
 import "./ContentContainer.scss";
 import Footer from "../../components/Footer";
 import Tutorial from "../Dashboard/Tutorial";
@@ -10,27 +10,23 @@ class ContentContainer extends Component {
     return (
       <div className="content-container">
         <header className="container">
-            <div className="prac ml-5">
-              <span>Practice</span>
-            </div>
+          <div className="prac ml-5">
+            <span>Practice</span>
+          </div>
           <div className="ml-5">
             <h1 className="heading">Dashboard</h1>
           </div>
           <div className=" d-flex flex-row-reverse">
-            <a className="bookmark" href="#">Bookmarked Challenges</a>
+            <a className="bookmark" href="#">
+              Bookmarked Challenges
+            </a>
           </div>
         </header>
         <div className="main-container">
           <div className="container">
-            <Fragment>
-              <Skills />
-            </Fragment>
-            <Fragment>
-              <Courses />
-            </Fragment>
-            <Fragment>
-              <Tutorial />
-            </Fragment>
+            <Skills />
+            <Courses />
+            <Tutorial />
           </div>
         </div>
         <Footer />
