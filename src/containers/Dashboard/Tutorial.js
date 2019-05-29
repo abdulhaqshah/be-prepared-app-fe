3ce95b567c7f0d9c6740bd8948772474255149b5
Humna <humna.aleem@southvillesol.com>
2019-05-29 12:18:41 +0500

    give one line space between functions

--- src/containers/Dashboard/Tutorial.js
+++ src/containers/Dashboard/Tutorial.js
@@ -1,39 +1,35 @@
-import React, { Component,Fragment } from "react";
+import React, { Component, Fragment } from "react";
 import "./Tutorial.scss";
 class Card extends Component {
   constructor(props) {
     super(props);
   }
+
   render() {
     return (
       <Fragment>
         <div>
-        <h5 className="headings ml-5 mb-5 mt-5">
-            Tutorials
-          </h5>
+          <h5 className="headings ml-5 mb-5 mt-5">Tutorials</h5>
         </div>
         <div className="row">
           <div className="tutorial-container">
             <div className="interview-prep-tutorial">
               <div className="tutorial-card shadow-lg ml-5 mr-4 mb-5">
-                <div
-                  className="card"
-                >
+                <div className="card">
                   <div className="card-body">
                     <h5 className="card-title">INTERVIEW PREPARATION</h5>
                     <h2 className="card-heading font-weight-bold">
                       Interview Preparation Kit
                     </h2>
                     <p>
-                      challenges based on learnings from 1000+ companies
-                      to help you prepare
+                      challenges based on learnings from 1000+ companies to help
+                      you prepare
                     </p>
                     <div className="tutorial-link">
-                    <a href="#" className=" font-weight-bold">
-                      View Tutorial
-                    </a>
+                      <a href="#" className=" font-weight-bold">
+                        View Tutorial
+                      </a>
                     </div>
-                   
                   </div>
                 </div>
               </div>
@@ -41,9 +37,7 @@ class Card extends Component {
 
             <div className="learn-code">
               <div className="tutorial-card shadow-lg ml-5 mr-2 mb-5">
-                <div
-                  className="card"
-                >
+                <div className="card">
                   <div className="card-body">
                     <p className="card-title">30 DAYS OF CODE</p>
                     <h2 className="card-heading font-weight-bold">
@@ -54,19 +48,17 @@ class Card extends Component {
                       and more.
                     </p>
                     <div className="tutorial-link">
-                  <a href="#" className="font-weight-bold">
-                    Start Tutorial
-                  </a>
-                  </div>
+                      <a href="#" className="font-weight-bold">
+                        Start Tutorial
+                      </a>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
             <div className="statistics">
               <div className="tutorial-card shadow-lg ml-5 mr-4 mb-5">
-                <div
-                  className="card"
-                >
+                <div className="card">
                   <div className="card-body">
                     <p className="card-title">TUTORIAL</p>
                     <h2 className="card-heading font-weight-bold">
@@ -76,34 +68,32 @@ class Card extends Component {
                       Learn Probabilitym distribution, regression and more!{" "}
                     </p>
                     <div className="tutorial-link">
-                  <a href="#" className="font-weight-bold">
-                    Start Tutorial
-                  </a>
-                  </div>
+                      <a href="#" className="font-weight-bold">
+                        Start Tutorial
+                      </a>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
             <div className="javascript">
-            <div className="tutorial-card shadow-lg  ml-5 mb-5">
-              <div className="card" >
-                <div className="card-body">
-                  <p className="card-title">TUTORIAL</p>
-                  <h2 className="card-heading font-weight-bold">
-                    10 Days of Javascript
-                  </h2>
-                  <p >
-                    Learn and improve your Javascript fundamentals"
-                  </p>
-                  <div className="tutorial-link">
-                  <a href="#" className="font-weight-bold">
-                    Start Tutorial
-                  </a>
+              <div className="tutorial-card shadow-lg  ml-5 mb-5">
+                <div className="card">
+                  <div className="card-body">
+                    <p className="card-title">TUTORIAL</p>
+                    <h2 className="card-heading font-weight-bold">
+                      10 Days of Javascript
+                    </h2>
+                    <p>Learn and improve your Javascript fundamentals"</p>
+                    <div className="tutorial-link">
+                      <a href="#" className="font-weight-bold">
+                        Start Tutorial
+                      </a>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
           </div>
         </div>
       </Fragment>
