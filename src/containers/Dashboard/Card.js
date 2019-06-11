0af758db60b5085c60015ee901d48850d582d5af
Humna <humna.aleem@southvillesol.com>
2019-06-11 12:20:37 +0500

    remove dummy data

--- src/containers/Dashboard/Card.js
+++ src/containers/Dashboard/Card.js
@@ -1,37 +1,28 @@
 import React, { Component } from "react";
-import {HOME} from '../../constants/index'
 import "./Card.scss";
 
 class Card extends Component {
-
+  constructor(props) {
+    super(props);
+  }
   render() {
+    const courses = ["OOP", "Algo", "Database"];
+    // const courses = this.props.data;
+    const courseList = courses.map(course => {
+      return (
+        <div className="course-name">
+          <a>{course}</a>
+        </div>
+      );
+    });
     return (
       <div className="courses-card">
-        <div  className="card shadow-lg">
+        <div className="card shadow-lg">
           <div className="card-body">
             <div className="header">
               <h5 className="card-title">{this.props.title}</h5>
             </div>
-            <div className="card-content">
-           <div className="course-name">
-                <a href={HOME}>OOP</a>
-              </div>
-              <div className="course-name">
-                <a href={HOME}>Data Structures</a>
-              </div>
-              <div className="course-name">
-                <a href={HOME}>Database</a>
-              </div>
-              <div className="course-name">
-                <a href={HOME}>Algorithm</a>
-              </div>
-              <div className="course-name">
-                <a href={HOME}>Web Development</a>
-              </div>
-              <div className="course-name">
-                <a href={HOME}>Android</a>
-              </div>
-            </div>
+            <div className="card-content">{courseList}</div>
           </div>
         </div>
       </div>
