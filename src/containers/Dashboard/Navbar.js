431b757f81364253e1140c663e26d05ea274a099
Humna <humna.aleem@southvillesol.com>
2019-06-12 13:26:17 +0500

    improve navbar responsivness

--- src/containers/Dashboard/Navbar.js
+++ src/containers/Dashboard/Navbar.js
@@ -11,7 +11,7 @@ class Navbar extends Component {
   render() {
     return (
       <nav className="navbar navbar-expand-lg fixed-top navbar-dark bg-dark shadow-lg">
-        <div>
+        <div className="logo-div">
           <Link className="navigation-logo" to={DASHBOARD}>
             Be Prepared
           </Link>
@@ -29,59 +29,63 @@ class Navbar extends Component {
         </button>
 
         <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-          <ul className="navbar-nav mr-auto mt-2 ml-3">
-            <li className="nav-item">
-              <a className="nav-link active" href={DASHBOARD}>
-                PRACTICE
-              </a>
-            </li>
-            <li className="nav-item ml-1">
-              <a className="nav-link" href={DASHBOARD}>
-                COMPETE
-              </a>
-            </li>
-            <li className="nav-item ml-1">
-              <a className="nav-link" href={DASHBOARD}>
-                JOBS
-              </a>
-            </li>
-            <li className="nav-item ml-1">
-              <a className="nav-link" href={DASHBOARD}>
-                LEADERBOARD
-              </a>
-            </li>
-          </ul>
-          <ul className="navbar-nav  mt-2 ml-5">
-            <li className="nav-item">
-              <form className="form-inline md-form form-sm mt-0">
-                <i className="fa fa-search" />
-                <input
-                  className=" form-control-sm ml-3 w-75"
-                  type="text"
-                  placeholder="Search"
-                  aria-label="Search"
-                />
-              </form>
-            </li>
+          <div className="links-div">
+            <ul className="navbar-nav mr-auto mt-2 ml-3">
+              <li className="nav-item">
+                <a className="nav-link active" href={DASHBOARD}>
+                  PRACTICE
+                </a>
+              </li>
+              <li className="nav-item ml-1">
+                <a className="nav-link" href={DASHBOARD}>
+                  COMPETE
+                </a>
+              </li>
+              <li className="nav-item ml-1">
+                <a className="nav-link" href={DASHBOARD}>
+                  JOBS
+                </a>
+              </li>
+              <li className="nav-item ml-1">
+                <a className="nav-link" href={DASHBOARD}>
+                  LEADERBOARD
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className="icons-div">
+            <ul className="navbar-nav  mt-2 ml-5">
+              <li className="nav-item">
+                <form className="form-inline md-form form-sm mt-0">
+                  <i className="fa fa-search" />
+                  <input
+                    className=" form-control-sm ml-3 w-75"
+                    type="text"
+                    placeholder="Search"
+                    aria-label="Search"
+                  />
+                </form>
+              </li>
 
-            <li className="nav-item ml-1">
-              <a className="nav-link" href={DASHBOARD}>
-                <i className="fa fa-comment-o fa-lg" />
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link " href={DASHBOARD}>
-                <i className="fa fa-bell-o fa-lg" />
-              </a>
-            </li>
-            <li className="nav-item ml-1">
-              <a className="nav-link" onClick={this.logout}>
-                <i className="fa fa-user-o fa-lg mr-1" />
-                {this.props.name}
-                <i className="fa fa-angle-down ml-1" />
-              </a>
-            </li>
-          </ul>
+              <li className="nav-item ml-1">
+                <a className="nav-link" href={DASHBOARD}>
+                  <i className="fa fa-comment-o fa-lg" />
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link " href={DASHBOARD}>
+                  <i className="fa fa-bell-o fa-lg" />
+                </a>
+              </li>
+              <li className="nav-item ml-1">
+                <a className="nav-link" onClick={this.logout}>
+                  <i className="fa fa-user-o fa-lg mr-1" />
+                  {this.props.name}
+                  <i className="fa fa-angle-down ml-1" />
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
       </nav>
     );
