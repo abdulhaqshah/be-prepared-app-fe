f627a5e34820ff2426ec1231e9d09aa0874c0709
Ahsan Yousaf <ahsan.yousaf@southvillesol.com>
2019-06-13 15:37:23 +0500

    Rename route file

--- src/routes/index.js
+++ src/routes/index.js
@@ -9,7 +9,7 @@ import ProblemSolving from "../containers/ProblemSolving/ProblemSolving";
 import Statistics from "../containers/Statistics/Statistics";
 import Javascript from "../containers/Javascript/Javascript";
 import Code from "../containers/Code/Code";
-import { ProtectedRoute,GuestRoute } from "./ProtectedRoute";
+import { ProtectedRoute, GuestRoute } from "./CustomRoute";
 import {
   HOME,
   SINGUP,
