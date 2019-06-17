5caf3c29c1b2cf02e4bd18823bc57560485c8b3c
Humna <humna.aleem@southvillesol.com>
2019-06-17 17:44:18 +0500

    made three cards in rightPane and align them

--- src/containers/UserProfile/RightPane.js
+++ src/containers/UserProfile/RightPane.js
@@ -5,20 +5,50 @@ class RightPane extends Component {
   render() {
     return (
       <Fragment>
-        <div className="row">
-          <div className="badges shadow-lg">
-            <div className="card">
-              <div className="card-body">
-                <div className="card-content">
-                  <h3 className="card-heading">Badges</h3>
-                  <div align="center">
-                    <i className="fa fa-id-badge fa-3x" />
-                    <div className="badge-text">
-                      <p className="card-text">
-                        You have not unlocked any badge yet
-                      </p>
-                    </div>
+        <div className="badges shadow-lg">
+          <div className="card">
+            <div className="card-body">
+              <div className="card-content">
+                <h5 className="card-heading">Badges</h5>
+                <div align="center">
+                  <i className="fa fa-id-badge fa-3x" />
+                  <div className="badge-text">
+                    <p className="card-text">
+                      You have not unlocked any badge yet
+                    </p>
                   </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="work-exp shadow-lg">
+          <div className="card">
+            <div className="card-body">
+              <div className="card-content">
+                <h5 className="card-heading">
+                  <i className="fa fa-briefcase fa-sm mr-2" />
+                  Work Experience
+                </h5>
+                <div className="work-exp-link">
+                  <a>
+                    + Add work experience, including contracts and internships
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="education shadow-lg">
+          <div className="card">
+            <div className="card-body">
+              <div className="card-content">
+                <h5 className="card-heading">
+                  <i className="fa fa-graduation-cap fa-sm mr-2" />
+                  Education
+                </h5>
+                <div className="badge-text">
+                  <a>+ Add your education history</a>
                 </div>
               </div>
             </div>
