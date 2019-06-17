73e7292ab82b69467c7a584fbe3fb056e5e024c6
Humna <humna.aleem@southvillesol.com>
2019-06-17 11:42:38 +0500

    set width of login-btn dv

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,18 +11,18 @@ class Header extends Component {
           <Link className="navigation-logo" to={HOME}>
             Be Prepared
           </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarTogglerDemo02"
+            aria-controls="navbarTogglerDemo02"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
         </div>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarTogglerDemo02"
-          aria-controls="navbarTogglerDemo02"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon" />
-        </button>
 
         <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
           <div className="login-btn">
