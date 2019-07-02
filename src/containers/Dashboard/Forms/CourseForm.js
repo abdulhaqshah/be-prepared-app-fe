94890b3fff632f622f43c9ff4b21fece890aa821
Humna <humna.aleem@southvillesol.com>
2019-07-02 12:03:45 +0500

    convert form from css into bootstrap

--- src/containers/Dashboard/Forms/CourseForm.js
+++ src/containers/Dashboard/Forms/CourseForm.js
@@ -2,8 +2,8 @@ import React, { Component } from "react";
 import SimpleReactValidator from "simple-react-validator";
 import ReactNotification from "react-notifications-component";
 import "react-notifications-component/dist/theme.css";
-// import addNotification from "../../utilities";
-// import API from "../../api";
+import addNotification from "../../../utilities/index";
+import API from "../../../api/index";
 import "./CourseForm.scss";
 
 class CourseForm extends Component {
@@ -12,12 +12,12 @@ class CourseForm extends Component {
 
     this.validator = new SimpleReactValidator();
     this.state = {
-      description: "",
-      content: ""
+      name: "",
+      description: ""
     };
     this.handleUserInput = this.handleUserInput.bind(this);
     this.notificationDOMRef = React.createRef();
-    // this.submitForm = this.submitForm.bind(this);
+    this.submitForm = this.submitForm.bind(this);
     this.formRef = null;
   }
 
@@ -26,52 +26,46 @@ class CourseForm extends Component {
     const value = e.target.value;
     this.setState({ [name]: value });
   }
-  // submitForm(e) {
-  //   e.preventDefault();
-  //   if (this.validator.allValid()) {
-  //     var { description, content } = this.state;
-  //     const data = {
-  //       tid: "656964e8-6787-41e2-bd1a-f4e21d007fd1",
-  //       topicId: "5cf4c9cff1571b101a2981ac",
-  //       lesson: {
-  //         description,
-  //         content
-  //       }
-  //     };
-  //     API.testData(data, result => {
-  //       if (result.status === "200") {
-  //         console.log(result.data);
-  //         //Form reset
-  //         this.formRef.reset();
-  //         //   this.props.history.push(DASHBOARD, this.state.email);
-  //       } else if (
-  //         result.status === "404" ||
-  //         result.status === "403" ||
-  //         result.status === "500"
-  //       ) {
-  //         addNotification(
-  //           this.notificationDOMRef,
-  //           "Error",
-  //           "danger",
-  //           result.message
-  //         );
-  //       } else {
-  //         addNotification(
-  //           this.notificationDOMRef,
-  //           "Error",
-  //           "warning",
-  //           "Somgthing went wrong"
-  //         );
-  //       }
-  //     }).catch = error => {
-  //       addNotification(this.notificationDOMRef, "Error", "warning", error);
-  //     };
-  //   } else {
-  //     this.validator.showMessages();
-  //     // rerender to show messages for the first time
-  //     this.forceUpdate();
-  //   }
-  // }
+  submitForm(e) {
+    e.preventDefault();
+    if (this.validator.allValid()) {
+      var { name, description } = this.state;
+      const data = {
+        name,
+        description
+      };
+      API.courseData(data, result => {
+        if (result.status === "200") {
+          // Form reset
+          this.formRef.reset();
+        } else if (
+          result.status === "404" ||
+          result.status === "403" ||
+          result.status === "500"
+        ) {
+          addNotification(
+            this.notificationDOMRef,
+            "Error",
+            "danger",
+            result.message
+          );
+        } else {
+          addNotification(
+            this.notificationDOMRef,
+            "Error",
+            "warning",
+            "Somgthing went wrong"
+          );
+        }
+      }).catch = error => {
+        addNotification(this.notificationDOMRef, "Error", "warning", error);
+      };
+    } else {
+      this.validator.showMessages();
+      // rerender to show messages for the first time
+      this.forceUpdate();
+    }
+  }
 
   render() {
     return (
@@ -79,8 +73,8 @@ class CourseForm extends Component {
         <div>
           <ReactNotification ref={this.notificationDOMRef} />
         </div>
-        <div className="container">
-          <div className="form-container">
+        <div className="d-flex justify-content-center container">
+          <div className="row">
             <div className="form-inner-container">
               <h1 className="heading" align="center">
                 Course Creation Form
@@ -90,9 +84,7 @@ class CourseForm extends Component {
                 ref={ref => (this.formRef = ref)}
               >
                 <div>
-                  <label className="labels vertical-spacing">
-                    Name <span className="form-required-indicator" />
-                  </label>
+                  <label className="labels">Name</label>
                   <br />
                   <input
                     className="form-field"
@@ -103,32 +95,29 @@ class CourseForm extends Component {
                   <div className="form-error-msg">
                     {this.validator.message(
                       "name",
-                      this.state.description,
+                      this.state.name,
                       "name"
                     )}
                   </div>
                 </div>
                 <div>
-                  <label className="form-field labels vertical-spacing">
-                    Description <span className="form-required-indicator" />{" "}
-                  </label>
+                  <label className="labels">Description</label>
                   <textarea className="form-control" rows="5" id="comment" />
+                  <div className="form-error-msg">
+                    {this.validator.message(
+                      "description",
+                      this.state.description,
+                      "description"
+                    )}
+                  </div>
                 </div>
-                <div className="row d-flex flex-row-reverse mt-5">
+                <div className="row d-flex flex-row-reverse mt-4">
                   <button
-                    className="btn btn-secondary col-lg-3 mt-2 ml-2 mr-2"
+                    className="btn btn-secondary col-lg-4 mt-1 mr-1 ml-1"
                     name="saveBtn"
                     type="submit"
                   >
                     Add Quiz
-                  </button>
-                  <button
-                    className="btn btn-secondary col-lg-3 mt-2"
-                    name="cancelBtn"
-                    type="submit"
-                    data-dismiss="modal"
-                  >
-                    Choose Image
                   </button>
                 </div>
               </form>
