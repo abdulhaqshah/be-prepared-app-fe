372ef9b8167a4bc088da86219facf926cd7aba06
Haris1994 <billichuha@hotmail.com>
2019-07-08 13:40:50 +0500

    Showed courses on dashboard after getting them from backend

--- src/containers/Dashboard/Courses/Courses.js
+++ src/containers/Dashboard/Courses/Courses.js
@@ -10,7 +10,7 @@ class Courses extends Component {
 
     this.state = {
       courses: [],
-      cards : ["", "", ""],
+      cards : ["Top Courses", "Most Viewed", "Top Rated"],
       course : []
     };
   }
@@ -18,27 +18,29 @@ class Courses extends Component {
   componentWillMount() {
     API.getCourses(result => {
       if (result.status === "200") {
-        console.log(result.data.length)
         this.setState({
-          course: result.data
+          courses: result.data
         });
-        if(this.state.course.length<18) {
-          let divider = this.state.course.length/3;
-          for(let i = 0; i<this.state.course.length; i++){
-            
-          }
-        } else if (this.state.course.length === 18) {
-
+        if (this.state.courses.length < 6) {
+          this.state.courses.map((course, index) => {
+            this.setState({
+              course: this.state.course.concat(course.name)
+            });
+          })
         } else {
-
+          for(let i =0 ; i<6; i++) {
+            this.setState({
+              course: this.state.course.concat(this.state.courses[i].name)
+            });
+          }
         }
       } else {
-        addNotification(
-          this.notificationDOMRef,
-          "Error",
-          "danger",
-          result.message
-        );
+        // addNotification(
+        //   this.notificationDOMRef,
+        //   "Error",
+        //   "danger",
+        //   result.message
+        // );
       }
     }).catch = error => {
       addNotification(this.notificationDOMRef, "Error", "warning", error);
@@ -55,7 +57,7 @@ class Courses extends Component {
           <div className="card-container">
           {this.state.cards.map((card, index) => (
             <div className="courses shadow-lg ml-5 mr-4 mb-5" key={index}>
-              <Card data={this.state.courses} title="COURSES" />
+              <Card data={this.state.course} key ={index} title={card} />
             </div>
           ))}
           </div>
