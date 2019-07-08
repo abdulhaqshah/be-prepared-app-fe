632829b65cc322cf3379ad0f778b641d0ce10073
Haris1994 <billichuha@hotmail.com>
2019-07-08 17:30:36 +0500

    Made quiz card component to show quizzes on their respective cards

---
+++ src/containers/Dashboard/Quizzes/QuizCard.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import "./Quiz.scss";
+
+class QuizCard extends Component {
+  
+  render() {
+    return (
+      <div className="card">
+        <div className="card-body">
+          <h5 className="card-title">
+            {this.props.title}
+            INTERVIEW PREPARATION
+          </h5>
+          <h2 className="card-heading font-weight-bold">
+            {this.props.heading}
+          </h2>
+          <p>
+            {this.props.para}
+            challenges based on learnings from 1000+ companies to help you
+            prepare
+          </p>
+          <div className="tutorial-link">
+            <a href={this.props.href} className="font-weight-bold">
+              View Quiz
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default QuizCard;
