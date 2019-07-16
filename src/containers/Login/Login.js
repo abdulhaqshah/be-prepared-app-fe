c939167160951b353657c55ac806ccf1205fbabe
Humna <humna.aleem@southvillesol.com>
2019-07-16 13:22:48 +0500

    remove parameters pass from router

--- src/containers/Login/Login.js
+++ src/containers/Login/Login.js
@@ -31,7 +31,7 @@ class Login extends Component {
         this.notificationDOMRef,
         "Success",
         "success",
-        this.props.location.state
+        "User has been login successfully"
       );
     }
   };
