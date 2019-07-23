c7024c3484e32e4ead7818e24854d36c046d490c
Humna <humna.aleem@southvillesol.com>
2019-07-23 18:57:29 +0500

    remove extra components

--- src/containers/InterviewPrep/InterviewPrep.js
+++
@@ -1,8 +0,0 @@
-import React, { Component } from "react";
-
-class InterviewPrep extends Component {
-  render() {
-    return <div>InterviewPrep Container</div>;
-  }
-}
-export default InterviewPrep;
