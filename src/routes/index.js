4c54b0334b12075648fe380b91f23518e4c28948
Humna <humna.aleem@southvillesol.com>
2019-07-23 18:58:49 +0500

    remove extra routes from index(routes)

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,12 +3,6 @@ import Home from "../containers/Home/Home";
 import SignUp from "../containers/SignUp/SignUp";
 import Login from "../containers/Login/Login";
 import Dashboard from "../containers/Dashboard/Dashboard";
-import Bookmark from "../containers/Bookmark/Bookmark";
-import InterviewPrep from "../containers/InterviewPrep/InterviewPrep";
-import ProblemSolving from "../containers/ProblemSolving/ProblemSolving";
-import Statistics from "../containers/Statistics/Statistics";
-import Javascript from "../containers/Javascript/Javascript";
-import Code from "../containers/Code/Code";
 import Profile from "../containers/UserProfile/UserProfile";
 import Navbar from "../containers/Dashboard/Navbar";
 import TutorialForm from "../containers/Dashboard/Forms/TutorialForm";
@@ -21,12 +15,6 @@ import {
   SINGUP,
   LOGIN,
   DASHBOARD,
-  BOOKMARK,
-  INTERVIEW_PREP_KIT,
-  PROBLEM_SOLVING,
-  CODE,
-  STATISTICS,
-  JAVASCRIPT,
   PROFILE,
   TUTORIAL_FORM,
   COURSE_FORM,
@@ -48,12 +36,6 @@ class Routes extends Component {
         />
         <ProtectedRoute path={DASHBOARD} component={Dashboard} />
         <ProtectedRoute path={PROFILE} component={Profile} />
-        <ProtectedRoute path={BOOKMARK} component={Bookmark} />
-        <ProtectedRoute path={INTERVIEW_PREP_KIT} component={InterviewPrep} />
-        <ProtectedRoute path={PROBLEM_SOLVING} component={ProblemSolving} />
-        <ProtectedRoute path={STATISTICS} component={Statistics} />
-        <ProtectedRoute path={JAVASCRIPT} component={Javascript} />
-        <ProtectedRoute path={CODE} component={Code} />
         <ProtectedRoute path={TUTORIAL_FORM} component={TutorialForm} />
         <ProtectedRoute path={COURSE_FORM} component={CourseForm} />
         <ProtectedRoute path={QUIZ_FORM} component={QuizForm} />
