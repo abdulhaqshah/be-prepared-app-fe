8a5565c5b92a13ab1b0c952e57a46a261a16c6fd
Humna <humna.aleem@southvillesol.com>
2019-07-23 19:01:10 +0500

    remove extra import and links from skills

--- src/containers/Dashboard/Skills/Skills.js
+++ src/containers/Dashboard/Skills/Skills.js
@@ -1,5 +1,4 @@
 import React, { Component, Fragment } from "react";
-import { INTERVIEW_PREP_KIT, PROBLEM_SOLVING } from "../../../constants";
 import "./Skills.scss";
 
 class Skills extends Component {
@@ -8,28 +7,25 @@ class Skills extends Component {
       <Fragment>
         <div className="skill-label ml-5 font-weight-bold">Your Skills</div>
         <div className="row">
-          <a href={INTERVIEW_PREP_KIT}>
-            <div className="interview-prep shadow-lg mt-5 mr-4 ml-5">
-              <div className="card">
-                <div className="card-body">
-                  <h5 className="card-title">INTERVIEW PREPARATION</h5>
-                  <div className="card-content">
-                    <h3 className="card-heading">Interview Preparation Kit</h3>
-                    <p className="card-text">
-                      curated challenges and tips based on learnings from 1000+
-                      compaanies to help you prepare for your upcoming
-                      interviews.
-                    </p>
-                    <div className="btn-div">
-                      <button className="card-btn shadow btn-success btn-lg">
-                        View
-                      </button>
-                    </div>
+          <div className="interview-prep shadow-lg mt-5 mr-4 ml-5">
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title">INTERVIEW PREPARATION</h5>
+                <div className="card-content">
+                  <h3 className="card-heading">Interview Preparation Kit</h3>
+                  <p className="card-text">
+                    curated challenges and tips based on learnings from 1000+
+                    compaanies to help you prepare for your upcoming interviews.
+                  </p>
+                  <div className="btn-div">
+                    <button className="card-btn shadow btn-success btn-lg">
+                      View
+                    </button>
                   </div>
                 </div>
               </div>
             </div>
-          </a>
+          </div>
           <div className="email shadow ml-5 mt-5">
             <div className="card bg-color">
               <div className="card-body">
@@ -58,26 +54,24 @@ class Skills extends Component {
               </div>
             </div>
           </div>
-          <a href={PROBLEM_SOLVING}>
-            <div className="prob-sol shadow-lg ml-5 mt-5">
-              <div className="card">
-                <div className="card-body">
-                  <p className="card-title">PROBLEM SOLVING</p>
-                  <h2 className="card-heading">Problem Solving</h2>
-                  <div className="progress-div">
-                    <div className="progress">
-                      <div className="progress-bar" role="progressbar" />
-                    </div>
+          <div className="prob-sol shadow-lg ml-5 mt-5">
+            <div className="card">
+              <div className="card-body">
+                <p className="card-title">PROBLEM SOLVING</p>
+                <h2 className="card-heading">Problem Solving</h2>
+                <div className="progress-div">
+                  <div className="progress">
+                    <div className="progress-bar" role="progressbar" />
                   </div>
-                  <div className="btn-div">
-                    <button className="card-btn shadow btn-outline-success btn-lg">
-                      Continue Practice
-                    </button>
-                  </div>
+                </div>
+                <div className="btn-div">
+                  <button className="card-btn shadow btn-outline-success btn-lg">
+                    Continue Practice
+                  </button>
                 </div>
               </div>
             </div>
-          </a>
+          </div>
         </div>
       </Fragment>
     );
