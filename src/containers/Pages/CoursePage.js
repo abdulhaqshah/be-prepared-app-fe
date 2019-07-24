52e20c4f1e4568ee945ef75bed3d8b1a4769b62e
Humna <humna.aleem@southvillesol.com>
2019-07-24 15:30:15 +0500

    add image on course screen page

--- src/containers/Pages/CoursePage.js
+++ src/containers/Pages/CoursePage.js
@@ -1,6 +1,14 @@
 import React, { Component } from "react";
+import image from "../../online-courses.jpeg";
+import "./CoursePage.scss";
 
 class CoursePage extends Component {
-  render() {}
+  render() {
+    return (
+      <div className="container">
+        <img src={image} className="image" alt="logo" />
+      </div>
+    );
+  }
 }
 export default CoursePage;
