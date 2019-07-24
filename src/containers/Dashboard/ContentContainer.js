0ea5ab85e1227efe7f8e03bb6fae4f39be262512
Humna <humna.aleem@southvillesol.com>
2019-07-24 12:09:31 +0500

    remove practice heading from dashboard

--- src/containers/Dashboard/ContentContainer.js
+++ src/containers/Dashboard/ContentContainer.js
@@ -2,19 +2,15 @@ import React, { Component } from "react";
 import "./ContentContainer.scss";
 import Footer from "../../components/Footer";
 import Tutorial from "../Dashboard/Tutorials/Tutorial";
-import Quiz from "../Dashboard/Quizzes/Quiz"
+import Quiz from "../Dashboard/Quizzes/Quiz";
 import Skills from "../Dashboard/Skills/Skills";
 import Courses from "./Courses/Courses";
-import { BOOKMARK } from "../../constants";
 
 class ContentContainer extends Component {
   render() {
     return (
       <div className="content-container">
         <header className="container">
-          <div className="prac ml-5">
-            <span>Practice</span>
-          </div>
           <div className="ml-5">
             <h1 className="heading">Dashboard</h1>
           </div>
