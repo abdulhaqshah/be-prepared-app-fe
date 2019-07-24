db7528ed863c88dfada374d437a7afa28f9508ca
Humna <humna.aleem@southvillesol.com>
2019-07-24 17:22:09 +0500

    set background image and made course content container

--- src/containers/Pages/CoursePage.js
+++ src/containers/Pages/CoursePage.js
@@ -6,9 +6,14 @@ import Footer from "../../components/Footer";
 class CoursePage extends Component {
   render() {
     return (
-      <div className="container">
-        <img src={image} className="image" alt="logo" />
-        <Footer/>
+      <div className="main-container">
+        <div className="container">
+          <img src={image} className="image" alt="logo" />
+          <div className="course-content-container">
+            <h3>Course Name</h3>
+          </div>
+          <Footer />
+        </div>
       </div>
     );
   }
