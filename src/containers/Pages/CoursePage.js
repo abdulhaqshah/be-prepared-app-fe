0e74e2382eb5391acd6d7c4bdee02e464deffa73
Humna <humna.aleem@southvillesol.com>
2019-07-27 18:28:52 +0500

    add card to show description of courses

--- src/containers/Pages/CoursePage.js
+++ src/containers/Pages/CoursePage.js
@@ -6,27 +6,29 @@ import Footer from "../../components/Footer";
 
 class CoursePage extends Component {
   render() {
+    console.log(this.props.course[0].description);
     return (
       <div className="main-course-container">
-        <div className="container">
-          <div className="zoom">
-            <img src={image} className="image" alt="logo" />
-          </div>
-          <div className="course-content-container">
-            <h3>{this.props.course[0].name}</h3>
-          </div>
-          <Footer />
+        <div className="header">
+          <h1>{this.props.course[0].name}</h1>
         </div>
+        <div className="course-content-container">
+          <div className="courses-detail-card">
+            <div className="card shadow-lg">
+              <div className="card-body">
+                <p>{this.props.course[0].description}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <Footer />
       </div>
     );
   }
 }
 const mapStateToProps = state => {
   return {
-    status: state.courseData.status,
     course: state.courseData.course
   };
 };
-export default connect(
-  mapStateToProps
-)(CoursePage);
+export default connect(mapStateToProps)(CoursePage);
