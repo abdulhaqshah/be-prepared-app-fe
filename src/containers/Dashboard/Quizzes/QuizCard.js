ea1c3d2a5b9f69311c703c80e8fffebe983aacc7
Haris1994 <billichuha@hotmail.com>
2019-07-30 19:15:28 +0500

    Added a new description field to quiz form

--- src/containers/Dashboard/Quizzes/QuizCard.js
+++ src/containers/Dashboard/Quizzes/QuizCard.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import LinesEllipsis from 'react-lines-ellipsis'
 import "./Quiz.scss";
 
 class QuizCard extends Component {
@@ -14,7 +15,14 @@ class QuizCard extends Component {
           <h2 className="card-heading font-weight-bold">
             {this.props.heading}
           </h2>
-          <p className="truncate-overflow">"challenges based on learnings from 1000+ companies to help you prepare"</p>
+          <LinesEllipsis
+            text='challenges based on learnings from 1000+ companies to help you prepare'
+            maxLine='2'
+            ellipsis='...'
+            trimRight
+            basedOn='letters'
+          />
+          {/* <p className="truncate-overflow">"challenges based on learnings from 1000+ companies to help you prepare"</p> */}
           <div className="quiz-link">
             <a href={this.props.href} className="font-weight-bold">
               View Quiz
