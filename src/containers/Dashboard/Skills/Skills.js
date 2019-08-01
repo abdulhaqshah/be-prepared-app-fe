269b50bb53ba7444f1f11871eec5a2784896549e
Haris1994 <billichuha@hotmail.com>
2019-08-01 15:19:08 +0500

    Made a skill card

--- src/containers/Dashboard/Skills/Skills.js
+++ src/containers/Dashboard/Skills/Skills.js
@@ -1,30 +1,25 @@
 import React, { Component, Fragment } from "react";
 import "./Skills.scss";
-
+import SkillCard from './SkillCard'
 class Skills extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tutorialProgress: [],
+      quizProgress : []
+    };
+    this.getUserProgress();
+  }
+
+  getUserProgress() {
+
+  }
   render() {
     return (
       <Fragment>
         <div className="skill-label ml-5 font-weight-bold">Your Skills</div>
-        <div className="row">
-          <div className="prob-sol shadow-lg ml-5 mt-5">
-            <div className="card">
-              <div className="card-body">
-                <p className="card-title">PROBLEM SOLVING</p>
-                <h2 className="card-heading">Problem Solving</h2>
-                <div className="progress-div">
-                  <div className="progress">
-                    <div className="progress-bar" role="progressbar" />
-                  </div>
-                </div>
-                <div className="btn-div">
-                  <button className="card-btn shadow btn-outline-success btn-lg">
-                    Continue Practice
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
+        <div>
+        <SkillCard/>
         </div>
       </Fragment>
     );
