0b6b86dec51d68d8e0b89f95042da03482d993bb
Haris1994 <billichuha@hotmail.com>
2019-08-02 19:54:47 +0500

    Shown user progress on profile page and set the css for it

--- src/containers/UserProfile/RightPanel.js
+++ src/containers/UserProfile/RightPanel.js
@@ -1,54 +1,106 @@
 import React, { Component, Fragment } from "react";
+import API from "../../api/index";
+import {Link,withRouter} from "react-router-dom"
+import { addNotification } from "../../utilities/index";
 import "./RightPanel.scss";
 
 class RightPane extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tutorialProgress: [],
+      quizProgress : []
+    };
+    API.getUserById(result => {
+      if (result.status === "200") {
+        let tutorialProgress = result.data.tutorialProgress.filter(obj => {
+          return (obj.completed) ? obj : null
+        });
+        tutorialProgress = tutorialProgress.slice(0, 3);
+        let quizProgress = result.data.quizProgress.filter(obj => {
+          return (obj.completed) ? obj : null
+        });
+        quizProgress = quizProgress.slice(0, 3);
+        this.setState({tutorialProgress, quizProgress});
+      }
+    }).catch = error => {
+      addNotification(this.notificationDOMRef, "Error", "warning", error);
+    };
+  }
+
+  getTutorialById = (tutorialId, name) => {
+    this.props.history.push(`/tutorial-page/${name}/${tutorialId}`);
+  };
   render() {
+    let tutorialCards, quizCards;
+    if(this.state.tutorialProgress.length === 0) {
+      tutorialCards = (
+          <div className="badge-text">
+            <p className="card-text">
+              You have not completed any tutorials
+            </p>
+          </div>
+      )
+    }else {
+      tutorialCards = this.state.tutorialProgress.map((tutorial, index) => (
+        <div class="flip-card">
+          <div class="flip-card-inner">
+            <div class="flip-card-front">
+              <p>{tutorial.name}</p>
+            </div>
+            <div class="flip-card-back">
+            <Link
+            onClick={this.getTutorialById.bind(this, tutorial.tutorialId, tutorial.name)} className="back">
+             View Tutorial
+            </Link>
+            </div>
+          </div>
+        </div>
+      ));
+    }
+    if(this.state.quizProgress.length === 0) {
+      quizCards = (
+        <div className="badge-text">
+          <p className="card-text">
+            You have not completed any quizzes
+          </p>
+        </div>
+    )
+    } else {
+      quizCards = this.state.quizProgress.map((quiz, index) => (
+        <div class="flip-card">
+          <div class="flip-card-inner">
+            <div class="flip-card-front">
+              <p>{quiz.name}</p>
+            </div>
+            <div class="flip-card-back">
+            <p>You have scored : {(quiz.correct/quiz.attempted) * 100}%</p>
+            </div>
+          </div>
+        </div>
+      ));
+    }
     return (
       <Fragment>
         <div className="badges shadow-lg">
           <div className="card">
             <div className="card-body">
               <div className="card-content">
-                <h5 className="card-heading">Badges</h5>
-                <div align="center">
-                  <i className="fa fa-id-badge fa-3x" />
-                  <div className="badge-text">
-                    <p className="card-text">
-                      You have not unlocked any badge yet
-                    </p>
-                  </div>
+                <h5 className="card-heading">Completed Tutorials</h5>
+                <div className="d-flex justify-content-around">
+                    {tutorialCards}
                 </div>
               </div>
             </div>
           </div>
         </div>
-        <div className="work-exp shadow-lg">
+        <div className="badges shadow-lg">
           <div className="card">
             <div className="card-body">
               <div className="card-content">
-                <h5 className="card-heading">
-                  <i className="fa fa-briefcase fa-sm mr-2" />
-                  Work Experience
-                </h5>
-                <div className="work-exp-link ml-4">
-                  <a href="./link">
-                    + Add work experience, including contracts and internships
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="education shadow-lg">
-          <div className="card">
-            <div className="card-body">
-              <div className="card-content">
-                <h5 className="card-heading">
-                  <i className="fa fa-graduation-cap fa-sm mr-2" />
-                  Education
-                </h5>
-                <div className="education-link ml-4">
-                  <a href="./link">+ Add your education history</a>
+                <h5 className="card-heading">Completed Quizzes</h5>
+                <div className="d-flex justify-content-around">
+                  {quizCards}
                 </div>
               </div>
             </div>
@@ -58,4 +110,4 @@ class RightPane extends Component {
     );
   }
 }
-export default RightPane;
+export default withRouter(RightPane);
